85aa00c5ceaa957d50de345b5ff0b17f2f3c0aef
balajidharma <balajidharma@hotmail.com>
2025-11-09 10:23:04 -0500

    Updated view table style

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,11 +1,11 @@
 <div class="w-full py-2">
-    <div class="min-w-full border-base-200 shadow">
-        <table class="table-fixed w-full text-sm">
+    <div class="overflow-x-auto">
+        <table class="table-auto w-full text-sm table-zebra">
             <tbody>
                 @foreach ($fields as $field)
                 @isset($item->display_values[$field['attribute']])
                 <tr>
-                    <td class="border-b border-slate-100 p-4 pl-8 text-slate-500">{{ $field['label'] ?? $field['attribute'] }}</td>
+                    <td class="border-b border-slate-100 p-4 text-slate-500">{{ $field['label'] ?? $field['attribute'] }}</td>
                     <td class="border-b border-slate-100 p-4 text-slate-500">{!! $item->display_values[$field['attribute']] !!}</td>
                 </tr>
                 @endisset
