8840c56f722529e70dc947a5c93a19d624ecb1cb
balajidharma <balajidharma@hotmail.com>
2025-11-20 19:19:39 -0500

    Added action buttons in view

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,32 @@
 <div class="w-full py-2">
     <div class="overflow-x-auto">
+        @canany(['adminUpdate', 'adminDelete'], $item)
+            <div class="flex justify-end gap-2 mt-2">
+                <form action="{{ $routes['destroy']($item->id) }}" method="POST">
+                    <div>
+                        @isset ($routes['edit'])
+                            @can('adminUpdate', $item)
+                                <a href="{{$routes['edit']($item->id)}}" class="btn btn-primary">Edit</a>
+                            @endcan
+                        @endisset
+
+                        @isset ($routes['destroy'])
+                            @can('adminDelete', $item)
+                                @csrf
+                                @method('DELETE')
+                                @if ($redirectUrl)
+                                    <input type="hidden" name="_redirect" value="{{ $redirectUrl }}">
+                                @endif
+                                <button class="btn btn-error"
+                                    onclick="return confirm('{{ __('Are you sure you want to delete?') }}')">
+                                    Delete
+                                </button>
+                            @endcan
+                        @endisset
+                    </div>
+                </form>
+            </div>
+        @endcanany
         <table class="table-auto w-full text-sm table-zebra">
             <tbody>
                 @foreach ($fields as $field)
